078cc05aa0a5fb3dde94ee8c1d25bcb30d87261c
Clement Delphin <clementcla@outlook.com>
2025-04-16 16:17:25 +0530

    changed music logo instead of letter S

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
+import { FaMusic } from "react-icons/fa6";
 
-function Header(){
+const Header : React.FC = () => {
     return (
         <header className='bg-blue-600 text-white shadow-md'>
             <div className='container mx-auto px-4 py-4 flex items-center'>
                 <div className='flex items-center'>
                     <div className='w-10 h-10 rounded-full bg-white text-blue-600 flex items-center justify-center font-bold text-xl mr-3'>
-                        S
+                        <FaMusic />
                     </div>
                     <h1 className='font-bold text-2xl'>Streamify Dashboard</h1>
                 </div>
